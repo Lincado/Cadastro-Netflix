c888927f2a827e3af82a79ccf3abb33ea113d777
Leo Alves <103528491+Lincado@users.noreply.github.com>
2022-10-07 17:41:02 -0300

    Add files via upload

--- assets/JS/accordion.js
+++ assets/JS/accordion.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -13,19 +12,3 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
-=======
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
->>>>>>> 4ef15ab2d8aa394de066d0f5c570cf454744f367
